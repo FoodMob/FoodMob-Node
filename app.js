dedd34d53f5eda25912b6c41c026c10485727c2f
Albert Shaw <ashaw596@gmail.com>
2016-02-07 17:41:03 -0500

    Fix up async calls to accurately return to client if registration was successful.

--- app.js
+++ app.js
@@ -1,84 +1,3 @@
-function main() {
-
-	function registerUser(email, password, first_name, last_name) {
-		console.log("Registering user: %s, password: %s, first_name: %s, last_name: %s", 
-			email, password, first_name, last_name);
-		//register User
-
-		var newUser = User({email: email, password: password, first_name: first_name, last_name: last_name});
-
-		console.log(newUser.email);
-
-	
-		newUser.save(function(err) {
-		  if (err) { throw err;
-			console.log(err);
-		  }
-
-		  console.log('User created!');
-		});
-		//if success
-		return true
-
-		//if failed
-		return false
-	}
-
-	var restify = require('restify');
-	 
-	var server = restify.createServer({
-	  name: 'myapp',
-	  version: '1.0.0'
-	});
-	server.use(restify.acceptParser(server.acceptable));
-	server.use(restify.queryParser());
-	server.use(restify.bodyParser());
-
-	server.on('uncaughtException', function (req, res, route, err) {
-	  console.log('uncaught error', err);
-	  return res.send(500, 'foo');
-	});
-	 
-	server.get('/echo/:name', function (req, res, next) {
-		//console.log(res);
-		//console.log(req)
-	  res.send(req.params);
-	  return next();
-	});
-
-	server.post('/echo/:name', function (req, res, next) {
-		console.log(req)
-		//console.log(res);
-		//console.log(req)
-	  //res.send(req.params);
-	  res.send(req.body);
-	  return next();
-	});
-
-	server.post('/user', function (req, res, next) {
-		params = req.params;
-		email = params.email;
-		first_name = params.first_name;
-		last_name = params.last_name;
-		password = params.password;
-
-		if (registerUser(email, password, first_name, last_name)) {
-			console.log(email + " register successfully");
-			res.send({"email": email, success: true});
-		} else {
-			console.log(email + " registration failed");
-			res.send({"email": email, success: false});
-		}
-
-	  	return next();
-	});
-
-
-	server.listen(8080, function () {
-	  console.log('%s listening at %s', server.name, server.url);
-	});
-};
-
 var mongoose = require('mongoose');
 
 // Connection URL. This is where your mongodb server is running.
@@ -110,3 +29,79 @@ mongoose.connect(url, function(err) {
 
 	main();
 });
+
+function main() {
+
+	//register User
+	function registerUser(email, password, first_name, last_name, callback) {
+		console.log("Registering user: %s, password: %s, first_name: %s, last_name: %s", 
+			email, password, first_name, last_name);
+
+		
+		var newUser = User({email: email, password: password, first_name: first_name, last_name: last_name});
+
+		console.log(newUser.email);
+
+		newUser.save(function(err) {
+		  if (err) { //throw err;
+			console.log(err);
+			callback(false);
+		  } else {
+		  	console.log('User created!');
+		  	callback(true);
+		  }
+		});
+	}
+
+	var restify = require('restify');
+	 
+	var server = restify.createServer({
+	  name: 'myapp',
+	  version: '1.0.0'
+	});
+	server.use(restify.acceptParser(server.acceptable));
+	server.use(restify.queryParser());
+	server.use(restify.bodyParser());
+
+	server.on('uncaughtException', function (req, res, route, err) {
+	  console.log('uncaught error', err);
+	  return res.send(500, 'foo');
+	});
+	 
+	server.get('/echo/:name', function (req, res, next) {
+		//console.log(res);
+		//console.log(req)
+	  res.send(req.params);
+	  return next();
+	});
+
+	server.post('/echo/:name', function (req, res, next) {
+		console.log(req.params)
+	  res.send(req.body);
+	  return next();
+	});
+
+	server.post('/user', function (req, res, next) {
+		params = req.params;
+		email = params.email;
+		first_name = params.first_name;
+		last_name = params.last_name;
+		password = params.password;
+
+		registerUser(email, password, first_name, last_name, function(worked) {
+			if (worked) {
+				console.log(email + " register successfully");
+				res.send({"email": email, success: true});
+			} else {
+				console.log(email + " registration failed");
+				res.send({"email": email, success: false});
+			}
+			next();
+		});
+	});
+
+
+	server.listen(8080, function () {
+	  console.log('%s listening at %s', server.name, server.url);
+	});
+};
--- client.js
+++ client.js
@@ -16,7 +16,7 @@ client.post('/echo/test', { hello: 'world', asdf: {test: 'test'} }, function(err
 ;
 */
 
-user_register_info = { email: 'ashaw596@gmail.com', password: "school", first_name: "Albert", last_name: "Shaw"};
+user_register_info = { email: 'test1@gmail.com', password: "school", first_name: "Albert", last_name: "Shaw"};
 
 client.post('/user', user_register_info, function(err, req, res, obj) {
   console.log(req);
@@ -25,4 +25,5 @@ client.post('/user', user_register_info, function(err, req, res, obj) {
   console.log('%j', obj);
   console.log(obj.asdf);
   console.log(obj);
+  client.close();
 });
